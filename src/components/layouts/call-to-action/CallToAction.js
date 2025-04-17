b69c45c34fa595e85026cc355c7074cdac4bf3d2
Corey Black <coreyblack@hotmail.co.uk>
2025-04-17 20:44:49 +0100

    Update CallToAction.js

--- src/components/layouts/call-to-action/CallToAction.js
+++ src/components/layouts/call-to-action/CallToAction.js
@@ -6,29 +6,45 @@ import { useShortcut } from '../../../context/ShortcutContext';
 
 import MainButton from '../../common/button/main/MainButton';
 
-const CallToAction = ({ titleBeforeBold, boldTitle, titleAfterBold, description }) => {
+const CallToAction = ({
+    titleBeforeBold,
+    boldTitle,
+    titleAfterBold,
+    description,
+    buttonText = "Explore work",
+    buttonLink
+}) => {
     const { openModal } = useConnectModal();
     const { shortcutToWork } = useShortcut();
 
-  	return (
+    const handleClick = () => {
+        if (buttonLink) {
+            window.open(buttonLink, '_blank');
+        } else if (shortcutToWork) {
+            shortcutToWork();
+        }
+    };
+
+    return (
         <div className="section column alternate">
             <div className="cta-content">
                 <div className="text-title">
                     {titleBeforeBold} <b>{boldTitle}</b> {titleAfterBold}
                 </div>
-                <div className={`text-description semibold`}>
+                <div className="text-description semibold">
                     {description}
                 </div>
                 <div className="cta-button-row">
-                    <MainButton 
+                    <MainButton
                         variant="black"
-                        text={"Explore work"}
-                        onClick={shortcutToWork}
+                        text={buttonText}
+                        onClick={handleClick}
                     />
                 </div>
             </div>
         </div>
     );
 };
+
 
 export default CallToAction;
